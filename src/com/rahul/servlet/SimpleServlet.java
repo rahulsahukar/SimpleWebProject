85f2eb70b8b2deb70a2150bb362cb9034091c3f8
Rahul Sahukar <rahul.sahukar@gmail.com>
2016-05-31 17:50:12 +0530

    Init commit

---
+++ src/com/rahul/servlet/SimpleServlet.java
@@ -0,0 +1,30 @@
+package com.rahul.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+@WebServlet(description = "First Servlet", urlPatterns = { "/SimpleServlet" })
+public class SimpleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("First Servlet!!!");
+		
+		PrintWriter printWriter = response.getWriter();
+		printWriter.println("<h1>My name is Rahul</h1>");
+		
+	}
+
+}
